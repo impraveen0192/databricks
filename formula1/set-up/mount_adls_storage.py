c81d5e36b2d50c70e14b59e4e4f5baa143699e9e
praveen.raj2010@gmail.com <praveen.raj2010@gmail.com>
2021-08-06 07:12:08 +0000

    set-up-notebook

---
+++ formula1/set-up/mount_adls_storage.py
@@ -0,0 +1,66 @@
+# Databricks notebook source
+storage_account_name = "pkformula1dl"
+client_id = "9c9f70f1-5a19-4b30-bdc0-792478630f89"
+tenant_id = "cce4b829-1804-453a-85f4-5abeacc01292"
+client_secret = "JYZtJ-LeQSa.Ti~60~07nBfOwJ3_99-wJ9"
+
+# COMMAND ----------
+
+configs = {"fs.azure.account.auth.type": "OAuth",
+           "fs.azure.account.oauth.provider.type":"org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+           "fs.azure.account.oauth2.client.id":f"{client_id}",
+           "fs.azure.account.oauth2.client.secret":f"{client_secret}",
+           "fs.azure.account.oauth2.client.endpoint": f"https://login.microsoftonline.com/{tenant_id}/oauth2/token"
+  
+ 
+}
+
+# COMMAND ----------
+
+container_name = "raw"
+dbutils.fs.mount(
+  source = f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/",
+  mount_point = f"/mnt/{storage_account_name}/{container_name}",
+  extra_configs=configs)
+
+# COMMAND ----------
+
+dbutils.fs.ls('/mnt/pkformula1dl/')
+
+# COMMAND ----------
+
+dbutils.fs.mounts()
+
+# COMMAND ----------
+
+container_name = "processed"
+dbutils.fs.mount(
+  source = f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/",
+  mount_point = f"/mnt/{storage_account_name}/{container_name}",
+  extra_configs=configs)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Create function to mount the containers
+
+# COMMAND ----------
+
+def mount_adls(container_name):
+  dbutils.fs.mount(
+    source = f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/",
+    mount_point = f"/mnt/{storage_account_name}/{container_name}",
+    extra_configs=configs)
+
+# COMMAND ----------
+
+#use above function to mount the gold container 
+mount_adls("gold")
+
+# COMMAND ----------
+
+dbutils.fs.ls("/mnt/")
+
+# COMMAND ----------
+
+
