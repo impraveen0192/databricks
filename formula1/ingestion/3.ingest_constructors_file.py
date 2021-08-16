b6ca33aef5aafdb4aa0671323ce3cecfd257c531
praveen.raj2010@gmail.com <praveen.raj2010@gmail.com>
2021-08-16 06:35:21 +0000

    3.ingest_constructors_file

--- formula1/ingestion/3.ingest_constructors_file.py
+++ formula1/ingestion/3.ingest_constructors_file.py
@@ -28,12 +28,50 @@ display(constructor_df)
 
 # COMMAND ----------
 
-### Useful when we want to  multiple dataframes and we want to specify the column of a specific dataframe. FOr example we have joined two dataframe and there is one comuln which is common in both the dataframes.
-constructor_dropped_df = constructor_df.drop(constructor_df['url'])
+from pyspark.sql.functions import col
 
 # COMMAND ----------
 
+##drop using col function
+##construct_dropped_df = constructor_df.drop(col('url'))
+
+# COMMAND ----------
+
+### Useful when we want to  multiple dataframes and we want to specify the column of a specific dataframe. For example we have joined two dataframe and there is one comuln which is common in both the dataframes.
+##constructor_dropped_df = constructor_df.drop(constructor_df['url'])
+
+# COMMAND ----------
+
+##Below can be useful when we want to drop multiple columns
 constructor_dropped_df = constructor_df.drop('url') 
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Step 3 - Rename columns and add ingestion date
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+
+# COMMAND ----------
+
+constructor_final_df = constructor_dropped_df.withColumnRenamed("constructorId","constructor_id") \
+                                            .withColumnRenamed("constructorRef","constructor_ref") \
+                                            .withColumn("ingestion_date",current_timestamp())
+
+# COMMAND ----------
+
+display(constructor_final_df)
+
+# COMMAND ----------
+
+constructor_final_df.write.mode("overwrite").parquet("/mnt/sapkformula1dl/processed/constructors")
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls /mnt/sapkformula1dl/processed/constructors
 
 # COMMAND ----------
 
