ed2dd3942e6d0991996644113339ef27ef35a846
praveen.raj2010@gmail.com <praveen.raj2010@gmail.com>
2021-09-01 22:42:09 +0000

    test change gith hub

--- Downstream/LVHV data ingestion.py
+++ Downstream/LVHV data ingestion.py
@@ -1,3 +1,15 @@
 # Databricks notebook source
+# MAGIC %run "../Downstream/includes/configuration"
+
+# COMMAND ----------
+
 # MAGIC %md 
 # MAGIC My name is praveen
+
+# COMMAND ----------
+
+display(spark.read.csv(f"{raw_folder_path}/circuits*"))
+
+# COMMAND ----------
+
+
